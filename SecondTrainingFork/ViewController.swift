880d97f6795b82aea1b15f6dfaf717830e58bfae
ivanrybkin <rybkin2517@mail.ru>
2023-05-22 21:08:29 +0300

    Initial Commit

---
+++ SecondTrainingFork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondTrainingFork
+//
+//  Created by Ivan Rybkin on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
